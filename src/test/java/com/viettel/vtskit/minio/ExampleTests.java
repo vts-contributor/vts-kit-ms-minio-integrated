51c27615058b8d4d2dbb2766afba0e2d1e63087c
phuhk <phuhk@viettel.com.vn>
2022-04-27 18:00:30 +0700

    - Configuration Repository

---
+++ src/test/java/com/viettel/vtskit/minio/ExampleTests.java
@@ -0,0 +1,13 @@
+package com.viettel.vtskit.minio;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTests {
+
+	@Test
+	public void example_Test() {
+		Assert.assertTrue("Pass Example Test", true);
+	}
+
+}
